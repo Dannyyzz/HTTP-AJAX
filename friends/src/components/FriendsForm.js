fd3f3eebc6c161c323a456de47e91c44f256cd4c
Dannyyzz <dannyzz@live.com>
2019-06-25 21:59:29 -0400

    Huge VSCode typo

--- friends/src/components/FriendsForm.js
+++ friends/src/components/FriendsForm.js
@@ -2,14 +2,28 @@ import React, { Component } from "react";
 import "./Friends.css";
 
 export default class FriendForm extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div className="form-wrapper">
-        <form onSubmit= {this.setState.props.addFriend} className="form-wrapper">
+        <form onSubmit={this.props.addFriend} className="form-wrapper">
           <div>
-            <input onChange={this.props.handleChanges} id="name" className="input" type="text" placeholder="Name" />
+            <input
+              onChange={this.props.handleChanges}
+              id="name"
+              className="input"
+              type="text"
+              placeholder="Name"
+            />
             <input id="age" className="input" type="text" placeholder="Age" />
-            <input id="email" className="input" type="email" placeholder="Email" />
+            <input
+              id="email"
+              className="input"
+              type="email"
+              placeholder="Email"
+            />
           </div>
           <div>
             <button>Add Friend</button>
