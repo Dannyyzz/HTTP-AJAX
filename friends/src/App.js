8bf9ac245d1813341c5228b97d3f0cdcf6d253a8
Dannyyzz <dannyzz@live.com>
2019-06-25 21:03:44 -0400

    Added .then && .catch to App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -14,6 +14,12 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    axios.get("http://localhost:5000/friends")
+    .then( res => this.setState({data: res.data}))
+    .catch( err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
