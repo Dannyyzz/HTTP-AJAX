67583f8eabe773ed16c339f8ddeffca57718f2b6
Dannyyzz <dannyzz@live.com>
2019-06-25 21:52:15 -0400

    Removed Update Button // Trying to make buttons functional

--- friends/src/App.js
+++ friends/src/App.js
@@ -10,23 +10,68 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: []
+      data: [],
+      name: "",
+      age: "",
+      email: "",
     };
   }
 
   componentDidMount() {
-    axios
-      .get("http://localhost:5000/friends")
-      .then(res => this.setState({ data: res.data }))
-      .catch(err => console.log(err));
+    axios.get("http://localhost:5000/friends")
+    .then( res => this.setState({data: res.data}))
+    .catch( err => console.log(err))
   }
+
+  addFriend = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/friends', {
+      name: (e.target.children[0].children[0].value),
+      age: (e.target.children[0].children[1].value),
+      email: (e.target.children[0].children[2].value),})
+      .then( res => {
+        this.setState ({
+          data: res.data
+        })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+      e.target.children[0].children[0].value = '';
+      e.target.children[0].children[1].value = '';
+      e.target.children[0].children[2].value = '';
+    }
+
+    deleteFriend = (e) => {
+      e.preventDefault();
+      console.log(e.target.children[0].innerText)
+      console.log(e.target.children[1].innerText)
+      console.log(e.target.children[2].innerText)
+
+      axios.delete('http://localhost:5000/friends', {
+          data: {
+          name: (e.target.children[0].innerText),
+          age: (e.target.children[1].innerText),
+          email: (e.target.children[2].innerText),
+        }})
+        .then( res => {
+          this.setState({
+            data: res.data
+          });
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
+  
 
   render() {
     return (
       <div className="App">
         <Header />
-        <Route path="/" render={props => <FriendsDisplay {...props} data={this.state.data} />}/>
-        <FriendsForm />
+        <Route path="/" render={props => <FriendsDisplay {...props} deleteFriend={this.deleteFriend} data={this.state.data} />}/>
+        <FriendsForm addFriend={this.addFriend} handleChanges={this.handleChanges} />
       </div>
     );
   }
--- friends/src/components/FriendsForm.js
+++ friends/src/components/FriendsForm.js
@@ -13,7 +13,6 @@ export default class FriendForm extends Component {
           </div>
           <div>
             <button>Add Friend</button>
-            <button>Update Friend</button>
             <button>Delete Friend</button>
           </div>
         </form>
