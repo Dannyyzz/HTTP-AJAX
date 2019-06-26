abbdb1878b43ac0c3258b17317b50761e35e45d6
Dannyyzz <dannyzz@live.com>
2019-06-25 21:01:54 -0400

    Added Imports && Routing in App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import axios from "axios";
 import Header from "./components/Header";
-import './App.css';
+import FriendsForm from "./components/FriendsForm";
+import FriendsDisplay from "./components/FriendsDisplay";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: [],
-    }
+      data: []
+    };
   }
 
-
-render() {
-  return (
-    <div className= "App">
-      <Header />
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Route path="/" render={props => <FriendsDisplay {...props} data={this.state.data}/>}/>
+        <FriendsForm />
+      </div>
+    );
+  }
 }
 
 export default App;
