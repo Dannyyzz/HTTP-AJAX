3d93f4e2469eed0fc3c72d0dd20367aeb20f8f78
Dannyyzz <dannyzz@live.com>
2019-06-25 21:57:09 -0400

    Refactoring Form with App.js

--- friends/src/components/FriendsForm.js
+++ friends/src/components/FriendsForm.js
@@ -5,15 +5,14 @@ export default class FriendForm extends Component {
   render() {
     return (
       <div className="form-wrapper">
-        <form className="form-wrapper">
+        <form onSubmit= {this.setState.props.addFriend} className="form-wrapper">
           <div>
-            <input className="input" type="text" placeholder="Name" />
-            <input className="input" type="text" placeholder="Age" />
-            <input className="input" type="email" placeholder="Email" />
+            <input onChange={this.props.handleChanges} id="name" className="input" type="text" placeholder="Name" />
+            <input id="age" className="input" type="text" placeholder="Age" />
+            <input id="email" className="input" type="email" placeholder="Email" />
           </div>
           <div>
             <button>Add Friend</button>
-            <button>Delete Friend</button>
           </div>
         </form>
       </div>
